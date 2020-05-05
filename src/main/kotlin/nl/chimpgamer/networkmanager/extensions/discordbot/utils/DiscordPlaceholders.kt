a98ee31f621ca838232a8ef86b988d7b8a6a8934
ChimpGamer <Tim.Cremers@Ziggo.nl>
2020-05-05 19:31:05 +0200

    Added a few new placeholders

--- src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/utils/DiscordPlaceholders.kt
+++ src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/utils/DiscordPlaceholders.kt
@@ -1,5 +1,6 @@
 package nl.chimpgamer.networkmanager.extensions.discordbot.utils
 
+import net.dv8tion.jda.api.OnlineStatus
 import nl.chimpgamer.networkmanager.api.models.player.Player
 import nl.chimpgamer.networkmanager.api.placeholders.PlaceholderHook
 import nl.chimpgamer.networkmanager.extensions.discordbot.DiscordBot
@@ -8,6 +9,10 @@ class DiscordPlaceholders(private val discordBot: DiscordBot) : PlaceholderHook(
     override fun onPlaceholderRequest(player: Player?, identifier: String): String? {
         when (identifier.toLowerCase()) {
             "users" -> return discordBot.discordUserManager.discordUsers.size.toString()
+            "guild_id" -> return discordBot.discordManager.guild.id
+            "guild_name" -> return discordBot.discordManager.guild.name
+            "guild_members_online" -> return discordBot.discordManager.guild.members.filter { it.onlineStatus === OnlineStatus.ONLINE }.size.toString()
+            "guild_members_total" -> return discordBot.discordManager.guild.memberCount.toString()
         }
         return null
     }
