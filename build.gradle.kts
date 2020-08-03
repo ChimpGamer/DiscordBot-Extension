f05f55ce68a8b04ebf7ba9cef22a45bab2e48b6b
ChimpGamer <Tim.Cremers@Ziggo.nl>
2020-08-03 18:08:30 +0200

    Bump to 1.3.4-SNAPSHOT and clener config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import org.apache.tools.ant.filters.ReplaceTokens
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     base
@@ -26,8 +25,8 @@ dependencies {
     implementation("net.kyori:text-api:3.0.2")
     implementation("com.jagrosh:jda-utilities-command:3.0.4")
     implementation("net.md-5:bungeecord-api:1.15-SNAPSHOT")
-    implementation("nl.chimpgamer.networkmanager:api:2.8.8-SNAPSHOT")
-    implementation("nl.chimpgamer.networkmanager:bungeecord:2.8.8-SNAPSHOT") {
+    implementation("nl.chimpgamer.networkmanager:api:2.8.9-SNAPSHOT")
+    implementation("nl.chimpgamer.networkmanager:bungeecord:2.8.9-SNAPSHOT") {
         exclude("org.bstats:bstats-bungeecord:1.7")
     }
     implementation("net.dv8tion:JDA:4.2.0_168")
@@ -37,39 +36,44 @@ dependencies {
 }
 
 group = "nl.chimpgamer.networkmanager.extensions"
-version = "1.3.3"
+version = "1.3.4-SNAPSHOT"
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-tasks.processResources {
-    val tokens = mapOf("version" to project.version)
-    from(sourceSets["main"].resources.srcDirs) {
-        filter<ReplaceTokens>("tokens" to tokens)
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
     }
-}
-
-tasks.shadowJar {
-    archiveFileName.set("${project.name}-v${project.version}.jar")
-
-    dependencies {
-        include(dependency("nl.chimpgamer.networkmanager.extensions:.*"))
-        include(dependency("com.jagrosh:.*"))
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
     }
 
-    relocate("kotlin", "nl.chimpgamer.networkmanager.lib.kotlin")
-    relocate("org.simpleyaml", "nl.chimpgamer.networkmanager.lib.simpleyaml")
-    relocate("org.eclipse.jetty", "nl.chimpgamer.networkmanager.lib.jetty")
-    relocate("javax.servlet", "nl.chimpgamer.networkmanager.lib.javax.servlet")
-    relocate("com.google.gson", "nl.chimpgamer.networkmanager.lib.gson")
-    relocate("com.jagrosh.jdautilities", "nl.chimpgamer.networkmanager.shaded.com.jagrosh.jdautilities")
-}
+    processResources {
+        val tokens = mapOf("version" to project.version)
+        from(sourceSets["main"].resources.srcDirs) {
+            filter<ReplaceTokens>("tokens" to tokens)
+        }
+    }
 
-tasks.build {
-    dependsOn(tasks.shadowJar)
-}
+    shadowJar {
+        archiveFileName.set("${project.name}-v${project.version}.jar")
 
-tasks.jar {
-    enabled = false
+        dependencies {
+            include(dependency("nl.chimpgamer.networkmanager.extensions:.*"))
+            include(dependency("com.jagrosh:.*"))
+        }
+
+        relocate("kotlin", "nl.chimpgamer.networkmanager.lib.kotlin")
+        relocate("org.simpleyaml", "nl.chimpgamer.networkmanager.lib.simpleyaml")
+        relocate("org.eclipse.jetty", "nl.chimpgamer.networkmanager.lib.jetty")
+        relocate("javax.servlet", "nl.chimpgamer.networkmanager.lib.javax.servlet")
+        relocate("com.google.gson", "nl.chimpgamer.networkmanager.lib.gson")
+        relocate("com.jagrosh.jdautilities", "nl.chimpgamer.networkmanager.shaded.com.jagrosh.jdautilities")
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
+    jar {
+        enabled = false
+    }
 }
