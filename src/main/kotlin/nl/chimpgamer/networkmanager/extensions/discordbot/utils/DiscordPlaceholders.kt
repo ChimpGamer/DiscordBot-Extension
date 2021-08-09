b59498479af3126eee028bf5c7a3c32157c4637d
ChimpGamer <Tim.Cremers@Ziggo.nl>
2021-08-09 19:32:15 +0200

    Added guild_boost_count placeholder

--- src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/utils/DiscordPlaceholders.kt
+++ src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/utils/DiscordPlaceholders.kt
@@ -13,9 +13,10 @@ class DiscordPlaceholders(private val discordBot: DiscordBot) : PlaceholderHook(
             "guild_name" -> return discordBot.discordManager.guild.name
             "guild_members_online" -> return discordBot.discordManager.guild.members.filter { it.onlineStatus === OnlineStatus.ONLINE }.size.toString()
             "guild_members_total" -> return discordBot.discordManager.guild.memberCount.toString()
+            "guild_boost_count" -> return discordBot.discordManager.guild.boostCount.toString()
             else -> {
-                if (parameters.startsWith("is_member_of_guild")) {
-                    val userId = parameters.replace("is_member_of_guild", "")
+                if (parameters.startsWith("is_member_of_guild_")) {
+                    val userId = parameters.replace("is_member_of_guild_", "")
                     val member = discordBot.discordManager.guild.getMemberById(userId)
                         ?: return "$userId is not a member of the discord server."
                     return "$userId (${member.asMention}) is a member of the discord server."
