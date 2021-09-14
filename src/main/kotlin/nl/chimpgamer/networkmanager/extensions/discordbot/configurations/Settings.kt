c5e4c40a2c738d83e33131c6c21f90289c8aa6f1
ChimpGamer <Tim.Cremers@Ziggo.nl>
2021-09-14 22:19:09 +0200

    Corrected path for login and firstlogin event settings

--- src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/configurations/Settings.kt
+++ src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/configurations/Settings.kt
@@ -90,6 +90,6 @@ enum class Setting(val path: String, val defaultValue: Any) {
     DISCORD_EVENTS_BUGREPORT_CHANNEL("bot.discord.events.bugreport.channel", "000000000000000000"),
     DISCORD_EVENTS_CHAT_CHANNELS("bot.discord.events.chat", mapOf("all" to "000000000000000000")),
     DISCORD_EVENTS_SERVERSTATUS_CHANNELS("bot.discord.events.serverStatus", mapOf("all" to "000000000000000000")),
-    DISCORD_EVENTS_LOGIN_CHANNEL("bot.discord.events.login", "000000000000000000"),
-    DISCORD_EVENTS_FIRST_LOGIN_CHANNEL("bot.discord.events.firstlogin", "000000000000000000")
+    DISCORD_EVENTS_LOGIN_CHANNEL("bot.discord.events.login.channel", "000000000000000000"),
+    DISCORD_EVENTS_FIRST_LOGIN_CHANNEL("bot.discord.events.firstlogin.channel", "000000000000000000")
 }
