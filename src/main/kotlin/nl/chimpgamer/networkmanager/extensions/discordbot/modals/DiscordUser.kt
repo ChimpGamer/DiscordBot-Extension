612b7085890968aef38cd1c910bfd2b1feef54c7
Zuydpool <2066387cremers@zuyd.nl>
2021-10-27 14:17:49 +0200

    Only import java.util.UUID

--- src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/modals/DiscordUser.kt
+++ src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/modals/DiscordUser.kt
@@ -1,5 +1,5 @@
 package nl.chimpgamer.networkmanager.extensions.discordbot.modals
 
-import java.util.*
+import java.util.UUID
 
 data class DiscordUser(val uuid: UUID, val discordId: String, val registered: Long = 0)
