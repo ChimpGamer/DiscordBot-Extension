6977d62397516f0333d48c682c9cbd936ae12809
ChimpGamer <Tim.Cremers@Ziggo.nl>
2022-05-19 15:38:56 +0200

    Dont return null when not in voicechannel in the %discord_user_current_voicechannel% placeholder

--- src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/utils/DiscordPlaceholders.kt
+++ src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/utils/DiscordPlaceholders.kt
@@ -11,7 +11,7 @@ class DiscordPlaceholders(private val discordBot: DiscordBot) : PlaceholderHook(
             "users" -> return discordBot.discordUserManager.discordUsers.size.toString()
             "guild_id" -> return discordBot.guild.id
             "guild_name" -> return discordBot.guild.name
-            "guild_members_online" -> return discordBot.guild.members.filter { it.onlineStatus === OnlineStatus.ONLINE }.size.toString()
+            "guild_members_online" -> return discordBot.guild.members.count { it.onlineStatus === OnlineStatus.ONLINE }.toString()
             "guild_members_total" -> return discordBot.guild.memberCount.toString()
             "guild_boost_count" -> return discordBot.guild.boostCount.toString()
             "is_registered" -> {
@@ -38,7 +38,7 @@ class DiscordPlaceholders(private val discordBot: DiscordBot) : PlaceholderHook(
                 if (player == null) return null
                 val userId = discordBot.discordUserManager.getDiscordIdByUuid(player.uuid) ?: return ""
                 val member = discordBot.guild.getMemberById(userId) ?: return "Member not found"
-                return member.voiceState?.channel?.name ?: "null"
+                return member.voiceState?.channel?.name ?: "Not in voice"
             }
             else -> {
                 if (parameters.startsWith("is_member_of_guild_")) {
