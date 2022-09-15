6d3fcbd67bb70f836781e0a6f24ffab9e5574043
ChimpGamer <=>
2022-09-15 20:41:22 +0200

    Set permission on networkmanagerbot command

--- src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/commands/mc/CloudNetworkManagerBotCommand.kt
+++ src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/commands/mc/CloudNetworkManagerBotCommand.kt
@@ -1,6 +1,7 @@
 package nl.chimpgamer.networkmanager.extensions.discordbot.commands.mc
 
 import cloud.commandframework.annotations.CommandMethod
+import cloud.commandframework.annotations.CommandPermission
 import nl.chimpgamer.networkmanager.api.models.player.Player
 import nl.chimpgamer.networkmanager.extensions.discordbot.DiscordBot
 import nl.chimpgamer.networkmanager.extensions.discordbot.configurations.MCMessage
@@ -8,18 +9,21 @@ import nl.chimpgamer.networkmanager.extensions.discordbot.configurations.MCMessa
 class CloudNetworkManagerBotCommand(private val discordBot: DiscordBot) {
 
     @CommandMethod("networkmanagerbot|nmbot reload config")
+    @CommandPermission("networkmanagerbot.command.reload")
     fun networkManagerBotCommandReloadConfig(player: Player) {
         discordBot.settings.reload()
         player.sendMessage(discordBot.messages.getString(MCMessage.RELOAD_CONFIG))
     }
 
     @CommandMethod("networkmanagerbot|nmbot reload messages")
+    @CommandPermission("networkmanagerbot.command.reload")
     fun networkManagerBotCommandReloadMessages(player: Player) {
         discordBot.messages.reload()
         player.sendMessage(discordBot.messages.getString(MCMessage.RELOAD_MESSAGES))
     }
 
     @CommandMethod("networkmanagerbot|nmbot reload jda")
+    @CommandPermission("networkmanagerbot.command.reload")
     fun networkManagerBotCommandReloadJDA(player: Player) {
         val success = discordBot.discordManager.restartJDA()
         if (success) {
