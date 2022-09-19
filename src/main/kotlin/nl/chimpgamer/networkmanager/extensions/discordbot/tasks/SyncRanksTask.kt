6b1e41d0f057f64013e01d6ca101ed67e48a688e
ChimpGamer <=>
2022-09-19 19:25:13 +0200

    Fixed more issues with syncing ranks

--- src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/tasks/SyncRanksTask.kt
+++ src/main/kotlin/nl/chimpgamer/networkmanager/extensions/discordbot/tasks/SyncRanksTask.kt
@@ -27,20 +27,26 @@ class SyncRanksTask(private val discordBot: DiscordBot, private val player: Play
                 discordBot.logger.warning("Could not find role $roleName to sync.")
                 continue
             }
+            if (role.isPublicRole) {
+                continue
+            }
+
             if (groups.any { it.equals(rankName, ignoreCase = true) }) {
                 addRoles.add(role)
             } else {
                 removeRoles.add(role)
             }
         }
+        // Remove roles from removeRoles that are already in addRoles
+        removeRoles.removeAll(addRoles)
+
         // remove roles that the user already has from roles to add
         addRoles.removeAll(member.roles.toSet())
+
         // remove roles that the user doesn't already have from roles to remove
         removeRoles.removeIf { role -> !member.roles.contains(role) }
         discordBot.networkManager.debug("AddRoles: $addRoles")
         discordBot.networkManager.debug("RemoveRoles: $removeRoles")
-        /*println("AddRoles: $addRoles")
-        println("RemoveRoles: $removeRoles")*/
         if (addRoles.isEmpty() && removeRoles.isEmpty()) {
             return
         }
