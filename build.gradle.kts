da59e2d61b80b10f4ea604b439cffe9e70c116f0
ChimpGamer <Tim.Cremers@Ziggo.nl>
2023-07-26 17:20:29 +0200

    Bump Kotlin and version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.8.22"
+    kotlin("jvm") version "1.9.0"
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2"
     //id("io.github.slimjar") version "1.3.0"
@@ -41,7 +41,7 @@ dependencies {
     compileOnly("net.md-5:bungeecord-api:1.16-R0.4")
     compileOnly("com.velocitypowered:velocity-api:3.1.1")
 
-    compileOnly("nl.chimpgamer.networkmanager:common-proxy:2.14.0-SNAPSHOT")
+    compileOnly("nl.chimpgamer.networkmanager:common-proxy:2.14.0")
 
     /*slim("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4") {
         exclude("org.jetbrains.kotlin")
@@ -51,7 +51,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-core:2.14.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.2")
 
-    implementation("net.dv8tion:JDA:5.0.0-beta.11") {
+    implementation("net.dv8tion:JDA:5.0.0-beta.12") {
         exclude("club.minnced", "opus-java")
         /*exclude("org.jetbrains.kotlin")
         exclude("org.jetbrains")
@@ -80,7 +80,7 @@ dependencies {
 }
 
 group = "nl.chimpgamer.networkmanager.extensions"
-version = "1.7.1-SNAPSHOT"
+version = "1.7.1"
 
 publishing {
     publications {
