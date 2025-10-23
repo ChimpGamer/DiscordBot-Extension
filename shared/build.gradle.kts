9badd8187b6a259f9eeb76f523777ba26df559e2
ChimpGamer <Tim.Cremers@Ziggo.nl>
2025-10-23 20:09:14 +0200

    Exclude kotlin from JDA and jda-ktx

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -9,7 +9,10 @@ dependencies {
 
     implementation("net.dv8tion:JDA:5.6.1") {
         exclude("club.minnced", "opus-java")
+        exclude("org.jetbrains.kotlin", "kotlin-stdlib")
     }
 
-    implementation("club.minnced:jda-ktx:0.12.0")
+    implementation("club.minnced:jda-ktx:0.12.0") {
+        exclude("org.jetbrains.kotlin", "kotlin-stdlib")
+    }
 }
